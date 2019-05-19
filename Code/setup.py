042b2d40c5ed6d8f9c902041c1fdb8533bd7d7b0
Matthew Aquilina <matthewaq31@gmail.com>
2019-05-19 17:52:48 +0100

    setup function.

---
+++ Code/setup.py
@@ -0,0 +1,10 @@
+""" Setup script for package. """
+from setuptools import setup
+
+setup(
+    name = "recognition-forge",
+    author = "Ari Anastassiou, Matthew Aquilina, Adam DePauw,
+    url = "https://github.com/mattaq31/recognition-forge.git",
+    packages=['nnframework']
+)
+
